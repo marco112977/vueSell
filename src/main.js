6b720ba38e41a4a3ef733cf8b22603fbdbeb0c0e
marco112977 <tanglijun1129@hotmail.com>
2018-10-25 18:43:35 +0800

    border-1px 样式修改

--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ Vue.config.productionTip = false
 import goods from 'components/goods/goods'
 import ratings from 'components/ratings/ratings'
 import seller from 'components/seller/seller'
+
+import 'common/stylus/index.styl'
+
   // 2. 定义路由
 // 每个路由应该映射一个组件。 其中"component" 可以是
 // 通过 Vue.extend() 创建的组件构造器，
@@ -30,6 +33,7 @@ const routes = [{
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
+  linkActiveClass: 'active',
   routes
 })
 // 4. 创建和挂载根实例。
